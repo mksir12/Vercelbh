6e87dbbe13fef3052474919943dc97db446e0d29
mksir12 <116490353+mksir12@users.noreply.github.com>
2025-09-04 17:56:44 +0530

    Update index.js

--- workers-site/index.js
+++ workers-site/index.js
@@ -1,11 +1,3 @@
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
-
-async function handleRequest(request) {
-  try {
-    return await fetch(new URL(request.url, `https://${request.headers.get('host')}`));
-  } catch (err) {
-    return new Response('Not found', { status: 404 });
-  }
-}
+  event.respondWith(fetch(event.request));
+});
